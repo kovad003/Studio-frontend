ff2ba6370cce45ac1ab54b1386be6e0bb698f2e5
Coder-AD-3000 <78084889+Coder-AD-3000@users.noreply.github.com>
2023-02-03 03:27:19 +0200

    Improved DashboardInfoCard icons

--- src/Components/DashboardInfo/DashboardInfoCard.js
+++ src/Components/DashboardInfo/DashboardInfoCard.js
@@ -2,61 +2,69 @@ import React from "react";
 import styled from "styled-components";
 
 const DashboardInfoCard = ({ icon, title, icon2, quantity, background }) => {
-	return (
-		<StyledDashboardCard>
-			<div className="DashboardCard__top">
-				<span
-					className="DashboardCard__icon"
-					style={{ backgroundColor: background }}
-				>
-					{icon}
-				</span>
-				<span className="DashboardCard__title">{title}</span>
-				<span className="DashboardCard__icon2">{icon2}</span>
-			</div>
-			<div className="DashboardCard__bottom">
-				<span className="DashboardCard__quantity">{quantity}</span>
-			</div>
-		</StyledDashboardCard>
-	);
+  return (
+    <StyledDashboardCard>
+      <div className="DashboardCard__top">
+        <span
+          className="DashboardCard__icon"
+          style={{ backgroundColor: background }}
+        >
+          {icon}
+        </span>
+        <span className="DashboardCard__title">{title}</span>
+        <span className="DashboardCard__icon2">{icon2}</span>
+      </div>
+      <div className="DashboardCard__bottom">
+        <span className="DashboardCard__quantity">{quantity}</span>
+      </div>
+    </StyledDashboardCard>
+  );
 };
 
 const StyledDashboardCard = styled.div`
-	height: 91px;
-	border-radius: 10px;
-	background-color: #fff;
-	flex: 1;
-	font-size: 16px;
-	.DashboardCard__top {
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		padding-top: 10px;
-	}
-	.DashboardCard__icon {
-		/* padding-top: 2px; */
-		padding: 2px 3px 2px 3px;
-		/*  background-color: ${(props) => props.theme.sidebarBtnHoverBg}; */
-		border-radius: 50%;
-	}
-	.DashboardCard__title {
-		margin-right: 10px;
-		margin-left: 10px;
-		color: ${(props) => props.theme.lightCardColor};
-	}
-	.DashboardCard__icon2 {
-		padding-top: 4px;
-		color: ${(props) => props.theme.lightInfoIcon};
-	}
-	.DashboardCard__bottom {
-		display: flex;
-		justify-content: center;
-		/*  align-items: center; */
-	}
-	.DashboardCard__quantity {
-		font-size: 38px;
-		/* font-weight: 700; */
-	}
+  height: 91px;
+  border-radius: 10px;
+  background-color: #fff;
+  flex: 1;
+  font-size: 16px;
+
+  // top section styling
+  .DashboardCard__top {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding-top: 10px;
+
+    .DashboardCard__icon {
+      /* padding: 5px 3px 3px 3px; */
+      /* border-radius: 50%; */
+      width: 26px;
+      height: 26px;
+      border-radius: 13px;
+      padding: 4px 2px 2px 4.5px;
+      /* margin-top: 1px; */
+    }
+
+    .DashboardCard__title {
+      margin-right: 10px;
+      margin-left: 10px;
+      color: ${(props) => props.theme.lightCardColor};
+    }
+    .DashboardCard__icon2 {
+      padding-top: 5px;
+      color: ${(props) => props.theme.lightInfoIcon};
+    }
+  }
+
+  // bottom section styling
+  .DashboardCard__bottom {
+    display: flex;
+    justify-content: center;
+
+    .DashboardCard__quantity {
+      font-size: 38px;
+    }
+  }
 `;
 
 export default DashboardInfoCard;
--- src/Components/DashboardInfo/data.js
+++ src/Components/DashboardInfo/data.js
@@ -1,32 +1,36 @@
 import React from "react";
 import { HiShoppingBag } from "react-icons/hi";
-import { AiFillInfoCircle } from "react-icons/ai";
+import {
+  AiFillInfoCircle,
+  AiOutlineDoubleRight,
+  AiTwotoneTrophy,
+} from "react-icons/ai";
 
 const data = [
-	{
-		id: "customers",
-		icon: <HiShoppingBag size={18} />,
-		title: "Customers",
-		icon2: <AiFillInfoCircle size={16} />,
-		quantity: 18,
-		background: "#E5F0FF",
-	},
-	{
-		id: "active",
-		icon: <HiShoppingBag size={18} />,
-		title: "Active",
-		icon2: <AiFillInfoCircle size={16} />,
-		quantity: 6,
-		background: "#FFDDB6",
-	},
-	{
-		id: "completed",
-		icon: <HiShoppingBag size={18} />,
-		title: "Completed",
-		icon2: <AiFillInfoCircle size={16} />,
-		quantity: 12,
-		background: "#A2DAB8",
-	},
+  {
+    id: "customers",
+    icon: <HiShoppingBag size={18} />,
+    title: "Customers",
+    icon2: <AiFillInfoCircle size={24} />,
+    quantity: 18,
+    background: "#E5F0FF",
+  },
+  {
+    id: "active",
+    icon: <AiOutlineDoubleRight size={18} />,
+    title: "Active",
+    icon2: <AiFillInfoCircle size={24} />,
+    quantity: 6,
+    background: "#FFDDB6",
+  },
+  {
+    id: "completed",
+    icon: <AiTwotoneTrophy size={18} />,
+    title: "Completed",
+    icon2: <AiFillInfoCircle size={24} />,
+    quantity: 12,
+    background: "#A2DAB8",
+  },
 ];
 
 export default data;
