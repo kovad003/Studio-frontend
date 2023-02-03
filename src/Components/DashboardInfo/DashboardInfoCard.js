f235a9d3cb5b5b92d0678e79e56768ff88812376
Coder-AD-3000 <78084889+Coder-AD-3000@users.noreply.github.com>
2023-02-03 14:34:38 +0200

    Creating a popper example test

--- src/Components/DashboardInfo/DashboardInfoCard.js
+++ src/Components/DashboardInfo/DashboardInfoCard.js
@@ -1,9 +1,38 @@
-import React from "react";
+/* import React from "react"; */
 import styled from "styled-components";
+import { usePopper } from "react-popper";
+import React, { useState, useRef } from "react";
+import classNames from "classnames";
 
 const DashboardInfoCard = ({ icon, title, icon2, quantity, background }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const boxRef = useRef();
+  const tooltipRef = useRef();
+
+  const { styles, attributes } = usePopper(boxRef.current, tooltipRef.current);
+
+  const onClickHeader = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <StyledDashboardCard>
+      <div ref={boxRef} className="box">
+        <p onClick={onClickHeader} className="title">
+          Click me!
+        </p>
+        <div>Dummy text</div>
+      </div>
+
+      <div
+        className={classNames("tooltip", { "tooltip-hidden": !isOpen })}
+        ref={tooltipRef}
+        style={styles.popper}
+        {...attributes.popper}
+      >
+        <div>an example tooltip</div>
+      </div>
+
       <div className="DashboardCard__top">
         <span
           className="DashboardCard__icon"
@@ -64,6 +93,20 @@ const StyledDashboardCard = styled.div`
     .DashboardCard__quantity {
       font-size: 38px;
     }
+  }
+
+  // Tooltip styles
+  .box .description-active {
+    display: block;
+  }
+
+  .tooltip {
+    background-color: indianred;
+  }
+
+  .tooltip-hidden {
+    visibility: hidden;
+    pointer-events: none;
   }
 `;
 
