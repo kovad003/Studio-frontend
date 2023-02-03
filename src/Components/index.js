982816c261e221e96c9d586253bc39ff1174d73a
Coder-AD-3000 <78084889+Coder-AD-3000@users.noreply.github.com>
2023-02-03 04:35:41 +0200

    fixed an import issue

--- src/Components/index.js
+++ src/Components/index.js
@@ -3,21 +3,21 @@ import Button from "./Button/Button";
 import InputWrapper from "./Input/InputWrapper";
 import Form from "./Form";
 import Sidebar from "./Sidebar/Sidebar";
-import Dashboard from "./Dashbord/Dashboard";
 import DashboardHeader from "./DashboardHeader/DashboardHeader";
 import Updates from "./Updates/Updates";
 import Users from "./Users/Users";
 import ProjectInfo from "./ProjectInfo/ProjectInfo";
+import Dashboard from "./Dashboard/Dashboard";
 
 export {
-	Button,
-	Card,
-	InputWrapper,
-	Form,
-	Sidebar,
-	Dashboard,
-	DashboardHeader,
-	Updates,
-	Users,
-	ProjectInfo,
+  Button,
+  Card,
+  InputWrapper,
+  Form,
+  Sidebar,
+  DashboardHeader,
+  Updates,
+  Users,
+  ProjectInfo,
+  Dashboard,
 };
