d6f365de38643a568265c5264d9e8d725c478821
Coder-AD-3000 <78084889+Coder-AD-3000@users.noreply.github.com>
2023-02-07 20:20:12 +0200

    added a temporary comment, to work on later

--- src/Components/Users/CreateUserPage.js
+++ src/Components/Users/CreateUserPage.js
@@ -1,7 +1,17 @@
-import React from "react";
+/* import React from "react";
+import { Form } from "react-router-dom";
+import InputWrapper from "../Input/InputWrapper";
 
 const CreateUserPage = () => {
-	return <div>CreateUserPage</div>;
+  return (
+    <div>
+      <Form>
+        <InputWrapper />
+        // accepts props, like placeholder prop, id, name, action, value, type
+      </Form>
+    </div>
+  );
 };
 
 export default CreateUserPage;
+ */
