b7063a95239c5d77bbea32ef203a39f488748886
Coder-AD-3000 <78084889+Coder-AD-3000@users.noreply.github.com>
2023-02-10 15:16:44 +0200

    created class names for the about page divs

--- src/Pages/AboutPage.js
+++ src/Pages/AboutPage.js
@@ -1,12 +1,29 @@
 import React from "react";
 import styled from "styled-components";
+import { Button } from "../Components";
 
 const AboutPage = () => {
-	return (
-		<div>
-			<h1>About page</h1>
-		</div>
-	);
+  return (
+    <StyledAboutPage>
+      <div className="AboutPage__header">
+        <div className="AboutPage__header-left"></div>
+
+        <div className="AboutPage__header-right">
+          <div className="AboutPage__btn">
+            <Button className="AboutPage__btn">Login</Button>
+          </div>
+        </div>
+      </div>
+
+      <h1>About page</h1>
+      <div className="AboutPage__content">
+        <div className="AboutPage__content-left"></div>
+        <div className="AboutPage__content-right"></div>
+      </div>
+
+      <div className="AboutPage__footer"></div>
+    </StyledAboutPage>
+  );
 };
 
 const StyledAboutPage = styled.section``;
