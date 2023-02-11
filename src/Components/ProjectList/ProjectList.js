7ce3d16a5b98d0aa291c3e96717484fdcb41d2ce
Cihan Erenler <cihanerenler@outlook.com>
2023-02-11 13:28:46 +0200

    get projects after login

--- src/Components/ProjectList/ProjectList.js
+++ src/Components/ProjectList/ProjectList.js
@@ -2,13 +2,18 @@ import React from "react";
 import styled from "styled-components";
 import data from "./dummy.data";
 import ProjectListItem from "./ProjectListItem";
+import useAppContext from "../../hooks/useAppContext";
 
 const ProjectList = () => {
+	const { projects } = useAppContext();
+
 	return (
 		<StyledProjectList>
-			{data.map((item) => {
-				return <ProjectListItem key={item.projectId} {...item} />;
-			})}
+			{projects
+				? projects.map((item) => {
+						return <ProjectListItem key={item.id} {...item} />;
+				  })
+				: "Loading..."}
 		</StyledProjectList>
 	);
 };
