b35659560dd2ada525f6079d09bb10a61ddbf40f
Coder-AD-3000 <78084889+Coder-AD-3000@users.noreply.github.com>
2023-02-11 02:39:52 +0200

    finished the about page header

--- src/Pages/AboutPage.js
+++ src/Pages/AboutPage.js
@@ -18,7 +18,34 @@ const AboutPage = () => {
 
       <div className="AboutPage__content">
         <div className="AboutPage__content-left">
-          <h1>About page</h1>
+          {/*  <h1>About page</h1> */}
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam at
+            felis mi. In rhoncus elementum ipsum sed varius. Nulla nec cursus
+            metus. Nam viverra eu neque at tempor. Nulla mauris nisl, euismod ut
+            eros sit amet, luctus faucibus dui. Nunc vitae velit in ipsum
+            pretium pharetra id in felis. Praesent pharetra fringilla tellus non
+            fermentum. Nunc ante libero, mollis eu tempor sed, consequat vitae
+            justo. Aenean mi magna, cursus id ornare ac, volutpat a purus.
+            Nullam dictum commodo neque, eget aliquet mi egestas in. Lorem ipsum
+            dolor sit amet, consectetur adipiscing elit. Aliquam dui nisi,
+            porttitor vitae purus eget, varius rhoncus mi. Mauris suscipit
+            interdum libero. Ut ultricies tortor arcu, a sollicitudin libero
+            molestie quis. Cras in feugiat sem. Morbi odio mi, pharetra nec
+            ligula id, aliquam accumsan nibh.
+          </p>
+
+          <p>
+            Integer ut scelerisque lectus, in sodales justo. Etiam eleifend
+            purus non massa semper tempor. Nulla mattis magna orci, nec
+            elementum mi placerat in. Nam ornare, arcu non imperdiet euismod,
+            enim ex tempor nunc, at varius enim dolor eget leo. Maecenas
+            vestibulum semper nunc, eu maximus tellus sollicitudin et. In et
+            mauris id arcu euismod ultrices sit amet accumsan metus. Mauris
+            ultrices eros vitae lacus auctor ultrices. Sed pretium dui ligula,
+            vitae consequat magna sodales vel. Maecenas auctor urna orci, eu
+            aliquet felis rutrum ornare.
+          </p>
         </div>
         <div className="AboutPage__content-right"></div>
       </div>
@@ -44,12 +71,7 @@ const StyledAboutPage = styled.section`
     background-color: ${(props) => props.theme.white};
     height: 60px;
 
-    .AboutPage__left {
-      flex: 1;
-      display: flex;
-      align-items: center;
-      justify-content: center;
-    }
+    /*  
 
     .AboutPage__right {
       display: flex;
@@ -57,12 +79,23 @@ const StyledAboutPage = styled.section`
       justify-content: center;
       width: 200px;
       padding: 0 10px;
-    }
+    } */
 
     .AboutPage__btn {
       width: 120px;
       height: 40px;
+      margin-right: 40px;
     }
+  }
+
+  .AboutPage__content {
+    flex: 1;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 18px 29px;
+    border-radius: 12px;
+    background-color: ${(props) => props.theme.white};
   }
 `;
 
