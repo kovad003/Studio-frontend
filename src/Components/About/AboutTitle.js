864ae6f75e5fdb75f8aa72e2f0f3482a49db5e74
Coder-AD-3000 <78084889+Coder-AD-3000@users.noreply.github.com>
2023-02-11 04:18:25 +0200

    working on the about page content

--- src/Components/About/AboutTitle.js
+++ src/Components/About/AboutTitle.js
@@ -2,14 +2,14 @@ import React from "react";
 import logo from "../../assets/logo.svg";
 import styled from "styled-components";
 
-const AboutLogo = () => {
-  return <StyledAboutLogo src={logo} alt="about-logo" />;
+const AboutTitle = () => {
+  return <StyledAboutTitle src={logo} alt="about-logo" />;
 };
 
-const StyledAboutLogo = styled.img`
+const StyledAboutTitle = styled.img`
   display: block;
   width: 150px;
   margin: 10px 20px;
 `;
 
-export default AboutLogo;
+export default AboutTitle;
--- src/Pages/AboutPage.js
+++ src/Pages/AboutPage.js
@@ -1,6 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 import { AboutLogo, Button } from "../Components";
+import AboutTitle from "../Components/About/AboutTitle";
+import About from "../assets/images/about-img-1.jpg";
 
 const AboutPage = () => {
   return (
@@ -16,38 +18,73 @@ const AboutPage = () => {
         </div>
       </div>
 
-      <div className="AboutPage__content">
-        <div className="AboutPage__content-left">
-          {/*  <h1>About page</h1> */}
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam at
-            felis mi. In rhoncus elementum ipsum sed varius. Nulla nec cursus
-            metus. Nam viverra eu neque at tempor. Nulla mauris nisl, euismod ut
-            eros sit amet, luctus faucibus dui. Nunc vitae velit in ipsum
-            pretium pharetra id in felis. Praesent pharetra fringilla tellus non
-            fermentum. Nunc ante libero, mollis eu tempor sed, consequat vitae
-            justo. Aenean mi magna, cursus id ornare ac, volutpat a purus.
-            Nullam dictum commodo neque, eget aliquet mi egestas in. Lorem ipsum
-            dolor sit amet, consectetur adipiscing elit. Aliquam dui nisi,
-            porttitor vitae purus eget, varius rhoncus mi. Mauris suscipit
-            interdum libero. Ut ultricies tortor arcu, a sollicitudin libero
-            molestie quis. Cras in feugiat sem. Morbi odio mi, pharetra nec
-            ligula id, aliquam accumsan nibh.
-          </p>
-
-          <p>
-            Integer ut scelerisque lectus, in sodales justo. Etiam eleifend
-            purus non massa semper tempor. Nulla mattis magna orci, nec
-            elementum mi placerat in. Nam ornare, arcu non imperdiet euismod,
-            enim ex tempor nunc, at varius enim dolor eget leo. Maecenas
-            vestibulum semper nunc, eu maximus tellus sollicitudin et. In et
-            mauris id arcu euismod ultrices sit amet accumsan metus. Mauris
-            ultrices eros vitae lacus auctor ultrices. Sed pretium dui ligula,
-            vitae consequat magna sodales vel. Maecenas auctor urna orci, eu
-            aliquet felis rutrum ornare.
-          </p>
+      <div className="AboutPage__content-outer">
+        <div className="AboutPage__content-title">
+          <AboutTitle />
         </div>
-        <div className="AboutPage__content-right"></div>
+        <div className="AboutPage__content-inner">
+          <div className="AboutPage__content-left">
+            {/*  <h1>About page</h1> */}
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam at
+              felis mi. In rhoncus elementum ipsum sed varius. Nulla nec cursus
+              metus. Nam viverra eu neque at tempor. Nulla mauris nisl, euismod
+              ut eros sit amet, luctus faucibus dui. Nunc vitae velit in ipsum
+              pretium pharetra id in felis. Praesent pharetra fringilla tellus
+              non fermentum. Nunc ante libero, mollis eu tempor sed, consequat
+              vitae justo. Aenean mi magna, cursus id ornare ac, volutpat a
+              purus. Nullam dictum commodo neque, eget aliquet mi egestas in.
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+              dui nisi, porttitor vitae purus eget, varius rhoncus mi. Mauris
+              suscipit interdum libero. Ut ultricies tortor arcu, a sollicitudin
+              libero molestie quis. Cras in feugiat sem. Morbi odio mi, pharetra
+              nec ligula id, aliquam accumsan nibh.
+            </p>
+
+            <p>
+              Integer ut scelerisque lectus, in sodales justo. Etiam eleifend
+              purus non massa semper tempor. Nulla mattis magna orci, nec
+              elementum mi placerat in. Nam ornare, arcu non imperdiet euismod,
+              enim ex tempor nunc, at varius enim dolor eget leo. Maecenas
+              vestibulum semper nunc, eu maximus tellus sollicitudin et. In et
+              mauris id arcu euismod ultrices sit amet accumsan metus. Mauris
+              ultrices eros vitae lacus auctor ultrices. Sed pretium dui ligula,
+              vitae consequat magna sodales vel. Maecenas auctor urna orci, eu
+              aliquet felis rutrum ornare.
+            </p>
+          </div>
+          <div className="AboutPage__content-right">
+            <img src={About} alt="About" />
+
+            {/*   <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam at
+              felis mi. In rhoncus elementum ipsum sed varius. Nulla nec cursus
+              metus. Nam viverra eu neque at tempor. Nulla mauris nisl, euismod
+              ut eros sit amet, luctus faucibus dui. Nunc vitae velit in ipsum
+              pretium pharetra id in felis. Praesent pharetra fringilla tellus
+              non fermentum. Nunc ante libero, mollis eu tempor sed, consequat
+              vitae justo. Aenean mi magna, cursus id ornare ac, volutpat a
+              purus. Nullam dictum commodo neque, eget aliquet mi egestas in.
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+              dui nisi, porttitor vitae purus eget, varius rhoncus mi. Mauris
+              suscipit interdum libero. Ut ultricies tortor arcu, a sollicitudin
+              libero molestie quis. Cras in feugiat sem. Morbi odio mi, pharetra
+              nec ligula id, aliquam accumsan nibh.
+            </p>
+            <br />
+            <p>
+              Integer ut scelerisque lectus, in sodales justo. Etiam eleifend
+              purus non massa semper tempor. Nulla mattis magna orci, nec
+              elementum mi placerat in. Nam ornare, arcu non imperdiet euismod,
+              enim ex tempor nunc, at varius enim dolor eget leo. Maecenas
+              vestibulum semper nunc, eu maximus tellus sollicitudin et. In et
+              mauris id arcu euismod ultrices sit amet accumsan metus. Mauris
+              ultrices eros vitae lacus auctor ultrices. Sed pretium dui ligula,
+              vitae consequat magna sodales vel. Maecenas auctor urna orci, eu
+              aliquet felis rutrum ornare.
+            </p> */}
+          </div>
+        </div>
       </div>
 
       <div className="AboutPage__footer"></div>
@@ -88,14 +125,47 @@ const StyledAboutPage = styled.section`
     }
   }
 
-  .AboutPage__content {
+  .AboutPage__content-outer {
     flex: 1;
     display: flex;
+    flex-direction: column;
     align-items: center;
     justify-content: center;
     margin: 18px 29px;
     border-radius: 12px;
     background-color: ${(props) => props.theme.white};
+
+    .AboutPage__content-title {
+      display: flex;
+      flex: 1;
+      background-color: red;
+    }
+
+    .AboutPage__content-inner {
+      display: flex;
+      background-color: yellow;
+    }
+
+    .AboutPage__content-left {
+      display: flex;
+      flex-direction: column;
+      background-color: green;
+      width: 50%;
+      padding: 30px;
+    }
+
+    .AboutPage__content-right {
+      display: flex;
+      flex-direction: column;
+      background-color: blue;
+      width: 50%;
+      padding: 30px;
+    }
+  }
+
+  p {
+    text-align: justify;
+    text-justify: inter-word;
   }
 `;
 
