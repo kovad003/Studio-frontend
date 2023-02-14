d7e9f5e0230cc07d706f7a39cc6713aa74ea7177
Coder-AD-3000 <78084889+Coder-AD-3000@users.noreply.github.com>
2023-02-14 23:27:50 +0200

    got the button navigation working for about

--- src/Components/About/AboutFooter.js
+++ src/Components/About/AboutFooter.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import styled from "styled-components";
 
@@ -6,7 +7,9 @@ const AboutFooter = () => {
   return (
     <StyledAboutFooter>
       <div className="AboutPage__footer">
-        <button className="AboutPage__footer-btn">Contact Us</button>
+        <Link to={"/contact"}>
+          <button className="AboutPage__footer-btn"> Contact Us</button>
+        </Link>
       </div>
     </StyledAboutFooter>
   );
